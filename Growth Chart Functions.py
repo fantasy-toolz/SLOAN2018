92d8e1cd69753b060219f75f9a28f56fe01cc147
Erich Rentz <rentz.erich@gmail.com>
2017-11-25 22:34:57 -0600

    GC Varying Dict Input
    Updated GC drawing functions to handle raw stats by calendar day and
    games played, note an error in percentiles associated with NAN

--- Growth Chart Functions.py
+++ Growth Chart Functions.py
@@ -12,11 +12,13 @@ import time
 import matplotlib as mpl
 import datetime
 from sklearn import linear_model
+from scipy.interpolate import spline
 from sklearn.metrics import mean_absolute_error
 
 min_GP = 50
 zero_day = 75
 max_day = 300
+graph_percentiles = [95, 90, 75, 50, 25, 10, 5]
 
 game_logs_dat = r"C:\Users\Erich Rentz\Documents\GitHub\SLOAN2018-master\data\gamelogs.dat"
 
@@ -51,7 +53,9 @@ def Read_DAT2Dict(in_file):
     f.close()
     return out_dict              
 
-def graph_growth_chart_1(in_player, in_stat, in_dict, in_denominator = None):
+def graph_growth_chart_1(in_player, in_stat, in_dict, in_denominator = None, draw_all_players = None):
+    # GP Length
+    GP_len = 0
     # Determine the percentile universe
     if in_denominator:
         # Create empty list for player indices
@@ -61,23 +65,37 @@ def graph_growth_chart_1(in_player, in_stat, in_dict, in_denominator = None):
         deno_query = in_denominator.values()[0]
         # Query based on input
         for indx,plr in enumerate(in_dict[deno_type].keys()):
+            y = np.cumsum(in_dict[in_stat][plr])
+            y = y[np.logical_not(np.isnan(y))]
+            if len(y) > GP_len:
+                GP_len = len(y)
             if len(np.where(in_dict[deno_type][plr] == deno_query)[0]) >= min_GP:
 #                print plr
                 plist.append(plr)
-        # Creat clean slate for player list then populate with stats for building percentiles
-        PAlist = np.zeros([len(plist),max_day-zero_day])
+        # Create clean slate for player list then populate with stats for building percentiles
+        PAlist = np.zeros([len(plist),GP_len])
         for indx,player in enumerate(np.array(plist)):
-            PAlist[indx] = np.cumsum(in_dict[in_stat][player])
+            PAlist[indx] = np.cumsum(in_dict[in_stat][player])[:GP_len]
     else:
-        PAlist = np.zeros([len(np.array(in_dict['PA'].keys())),max_day-zero_day])
+        for indx,plr in enumerate(in_dict['PA'].keys()):
+            y = np.cumsum(in_dict[in_stat][plr])
+            y = y[np.logical_not(np.isnan(y))]
+            if len(y) > GP_len:
+                GP_len = len(y)
+        PAlist = np.zeros([len(np.array(in_dict['PA'].keys())),GP_len])
         # For Player in Stat Keys, fill empty slate
         for indx,player in enumerate(in_dict[in_stat].keys()):
-            PAlist[indx] = np.cumsum(in_dict[in_stat][player])
+            PAlist[indx] = np.cumsum(in_dict[in_stat][player])[:GP_len]
+#    print "This is the GP: {0}".format(GP_len)
     # Create Grap Object and add axes
     fig = plt.figure()
     ax = fig.add_axes([0.18,0.22,0.6,0.7])
     ax2 = fig.add_axes([0.81,0.22,0.02,0.7])
+    if draw_all_players:
+        for index in PAlist:
+            ax.plot(index,color='grey')
     # Draw Percentiles
+#    for perc in graph_percentiles:
     for perc in range(100,0,-10):
         ax.plot(np.percentile(PAlist,perc,axis=0),color=cm.gnuplot(float(perc/100.),1.))
     # Draw Player Profile
@@ -90,12 +108,13 @@ def graph_growth_chart_1(in_player, in_stat, in_dict, in_denominator = None):
     cb1.set_label('Percentile',size=18)
 
 def graph_growth_chart_2(in_player, in_stat, in_dict, gen_fig=True): 
-    # Grab Stats
-    x = np.array(range(0, max_day-zero_day)) 
+    # Grab Stats 
     y = np.cumsum(in_dict[in_stat][in_player])
+    y = y[np.logical_not(np.isnan(y))]
+    x = np.array(range(0,len(y))) 
     # Reshape the Data
-    x = x.reshape(max_day-zero_day, 1)
-    y = y.reshape(max_day-zero_day, 1)
+    x = x.reshape(len(y), 1)
+    y = y.reshape(len(y), 1)
     # Create linear regression object
     regr = linear_model.LinearRegression()
     # Fit a line to Data
@@ -135,11 +154,12 @@ def graph_growth_chart_2(in_player, in_stat, in_dict, gen_fig=True):
 
 def graph_growth_chart_3(in_player, in_stat, in_dict, gen_fig=True, window_size = 7):   
     # Grab Stats 
-    x = np.array(range(0, max_day-zero_day)) 
     y = np.cumsum(in_dict[in_stat][in_player])
+    y = y[np.logical_not(np.isnan(y))]
+    x = np.array(range(0,len(y))) 
     # Reshape the Data
-    x = x.reshape(max_day-zero_day, 1)
-    y = y.reshape(max_day-zero_day, 1)
+    x = x.reshape(len(y), 1)
+    y = y.reshape(len(y), 1)
     # Create Base Plot and scatter
     fig = plt.figure()
     reg_plot = fig.add_subplot(111)
@@ -148,10 +168,10 @@ def graph_growth_chart_3(in_player, in_stat, in_dict, gen_fig=True, window_size 
     regr = linear_model.LinearRegression()
     reg_list = []
     # Calculate last day and define start date
-    final_day = max_day-zero_day
-    premier_date = 18
+    final_day = len(y)
+    premier_date = 14
     # Run through days creating a moving window linear regression across the longitudinal data
-    for day in range(premier_date, max_day-zero_day, window_size):
+    for day in range(premier_date, len(y), window_size):
         regr = linear_model.LinearRegression()
         start_day = day
         stop_day = day+7
@@ -168,12 +188,12 @@ def graph_growth_chart_3(in_player, in_stat, in_dict, gen_fig=True, window_size 
         start_day = day-(window_size+2)
         stop_day = day + (window_size+2)
         if stop_day < final_day:
-             sub_x = x[start_day:stop_day]
-             sub_y = y[start_day:stop_day]
-             sub_x = sub_x.reshape(start_day-stop_day+1,1)
-             sub_y = sub_y.reshape(start_day-stop_day+1,1)
-             reg_plot.plot(sub_x, lm.predict(sub_x), color='red', linewidth=1, linestyle = '-')
-             day = day+window_size
+            sub_x = x[start_day:stop_day]
+            sub_y = y[start_day:stop_day]
+            sub_x = sub_x.reshape(start_day-stop_day+1,1)
+            sub_y = sub_y.reshape(start_day-stop_day+1,1)
+            reg_plot.plot(sub_x, lm.predict(sub_x), color='red', linewidth=1, linestyle = '-')
+            day = day+window_size
     # Add some graph junk
     plt.tick_params(
             axis='x',
@@ -210,7 +230,7 @@ for key1 in DDStats.keys():
             
         else:
             GStats[key1][key2] = np.zeros(max_day-zero_day) + np.nan
-            
+#            GStats[key1][key2] = np.zeros(max_day-zero_day) + 0
         gnum = 0
         for indx,val in enumerate(DDStats[key1][key2]):
             
@@ -219,12 +239,12 @@ for key1 in DDStats.keys():
                 gnum += 1
 
 # Create Growth Chart 1 Samples
-graph_growth_chart_1('Adam Jones', 'R',  DDStats)
-temp_dict = {'POS':'CF'}
-graph_growth_chart_1('Adam Jones', 'R',  DDStats, temp_dict)
+graph_growth_chart_1('Adam Jones', 'R',  GStats)
+temp_dict = {'POS':'1B'}
+graph_growth_chart_1('Joey Votto', 'R', GStats, temp_dict)
 
 # Create Growth Chart 2 Samples
-graph_growth_chart_2('Adam Jones', 'R',  DDStats, gen_fig=True)
+graph_growth_chart_2('Adam Jones', 'R',  GStats, gen_fig=True)
 graph_growth_chart_2('Jose Altuve', 'R',  DDStats, gen_fig=True)
 graph_growth_chart_2('Alcides Escobar', 'R',  DDStats, gen_fig=True)   
 graph_growth_chart_2('Corey Dickerson', 'R',  DDStats, gen_fig=True)
